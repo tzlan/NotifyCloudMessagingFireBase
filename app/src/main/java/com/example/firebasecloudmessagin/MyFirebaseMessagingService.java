73edfffcfb9e2a42b9245583c26dc2ae00c15e96
TalZ <talzana55@gmail.com>
2022-03-23 14:58:08 +0100

    Firs

---
+++ app/src/main/java/com/example/firebasecloudmessagin/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.example.firebasecloudmessagin;
+
+import android.app.Service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
